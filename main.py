eaf6949f2ea6c7af03be302fb44c664e48c76ad9
Aayush Chadha <mbaxaac2@manchester.ac.uk>
2020-01-18 18:59:07 +0000

    Add threading

--- main.py
+++ main.py
@@ -5,21 +5,28 @@ import picamera
 import requests
 import io
 
+video_stream = io.BytesIO()
+image_stream = io.BytesIO()
+lock = Lock()
+
 def emotion_rec():
-	video_stream = io.BytesIO()
-	image_stream = io.BytesIO()
 	with picamera.PiCamera() as camera:
 		camera.resolution = (640,480)
-		camera.start_recording(stream, format='h264', quality=23)
-		camera.wait_recording(2)
-		camera.capture(image_stream, use_video_port=True)
-		make_request(image_stream)
-		camera.wait_recording(2)
-		camera.stop_recording()
-	def make_request(image_stream):
-		request_url = 'https://hackcambridge-emotiondetector.cognitiveservices.azure.com/face/v1.0/detect?overload=stream'
-		headers = {'apiKey':'165fbaf18f434fd3a52fca8890ca2800'}
-		request = requests.post(request_url, headers=headers, data=stream)
-		print (request.json())	
+		camera.start_recording(video_stream, format='h264', quality=23)
+		start_time = 
+		while :
+			camera.wait_recording(2)
+			camera.capture(image_stream, use_video_port=True)
+			# Spawn a separate thread for the request so video is not blocked
+			Thread(target=make_request, args=(image_stream,))
+			camera.wait_recording(2)
+			camera.stop_recording()
+
+def make_request(image_stream):
+	lock
+	request_url = 'https://hackcambridge-emotiondetector.cognitiveservices.azure.com/face/v1.0/detect?overload=stream'
+	headers = {'apiKey':'165fbaf18f434fd3a52fca8890ca2800'}
+	request = requests.post(request_url, headers=headers, data=stream)
+	print (request.json())	
 
 emotion_rec()
