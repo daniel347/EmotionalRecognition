b37690accbd3a2d608b948cc65323471f5bc3034
Aayush Chadha <mbaxaac2@manchester.ac.uk>
2020-01-18 22:02:02 +0000

    Add working requests sans correct image

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import io
 video_stream = io.BytesIO()
 image_stream = io.BytesIO()
 lock = Lock()
+file_counter = 0
 
 def emotion_rec():
 	with picamera.PiCamera() as camera:
@@ -17,15 +18,12 @@ def emotion_rec():
 		camera.start_recording(video_stream, format='h264', quality=23)
 		start_time = time.time()
 		while (time.time() - start_time) < 10:
-			camera.wait_recording(2)
-			camera.capture(image_stream, use_video_port=True, format='jpeg')
-			with open("testfile.jpeg", 'wb') as fp1:
-				image_stream.seek(0)
-				file = image_stream.read()
-				fp1.write(file)
-				exit()
-			# Spawn a separate thread for the request so video is not blocked
-			Thread(target=make_request, args=(image_stream,)).start()
+			if(lock.acquire(blocking=)):
+				camera.wait_recording(2)
+				camera.capture(image_stream, use_video_port=True, format='jpeg')
+				# Spawn a separate thread for the request so video is not blocked
+				lock.release()
+				Thread(target=make_request, args=(image_stream,)).start()
 		camera.stop_recording()
 		camera.stop_preview()
 
@@ -37,18 +35,23 @@ def get_request_params():
 	return request_params
 
 def make_request(image_stream):
+	global file_counter
 	print ("Making_request")
 	lock_on_stream = lock.acquire()
 	request_params = get_request_params()
-	with open("image.jpeg", "wb") as fp:
-		file = image_stream.read()
-		fp.write(file)
 	try:
+		image_stream.seek(0)
+		with open("test_file_{0}.jpeg".format(file_counter), "wb") as fp:
+			image_stream.seek(0)
+			file = image_stream.read()
+			fp.write(file)
+			file_counter+=1
+		image_stream.seek(0)
 		request_url = 'https://hackcambridge-emotiondetector.cognitiveservices.azure.com/face/v1.0/detect'
 		request = requests.post(request_url, params=request_params['request_data'], headers=request_params['headers'], data=image_stream)
 		print (request.json())
-		image_stream.flush()
 	finally:
+		image_stream.flush()
 		lock_on_stream = lock.release()
 
 emotion_rec()
