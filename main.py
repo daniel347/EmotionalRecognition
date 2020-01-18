07a0f98c907b61f9f7fe518f0371545410f56cf7
Aayush Chadha <mbaxaac2@manchester.ac.uk>
2020-01-18 18:43:45 +0000

    Add camera capture

--- main.py
+++ main.py
@@ -6,15 +6,20 @@ import requests
 import io
 
 def emotion_rec():
-	stream = io.BytesIO()
+	video_stream = io.BytesIO()
+	image_stream = io.BytesIO()
 	with picamera.PiCamera() as camera:
 		camera.resolution = (640,480)
 		camera.start_recording(stream, format='h264', quality=23)
-		camera.wait_recording(5)
+		camera.wait_recording(2)
+		camera.capture(image_stream, use_video_port=True)
+		make_request(image_stream)
+		camera.wait_recording(2)
 		camera.stop_recording()
-	request_url = 'https://hackcambridge-emotiondetector.cognitiveservices.azure.com/face/v1.0/detect?overload=stream&returnFaceAttributes=["smile", "emotion]'
-	print (stream)
-	request = requests.post(request_url, data=stream)
-	print (request.json())	
+	def make_request(image_stream):
+		request_url = 'https://hackcambridge-emotiondetector.cognitiveservices.azure.com/face/v1.0/detect?overload=stream'
+		headers = {'apiKey':'165fbaf18f434fd3a52fca8890ca2800'}
+		request = requests.post(request_url, headers=headers, data=stream)
+		print (request.json())	
 
 emotion_rec()
