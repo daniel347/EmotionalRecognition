67979b863d46c3ba24447843ca87568915d5a413
Aayush Chadha <achadharocks@gmail.com>
2020-01-18 22:26:42 +0000

    Add queue based operations

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import cv2
 import picamera
 import requests
 import io
+from queue import *
 
 video_stream = io.BytesIO()
 image_stream = io.BytesIO()
 lock = Lock()
-file_counter = 0
+
+image_queue = LifoQueue(maxsize=100)
 
 def emotion_rec():
 	with picamera.PiCamera() as camera:
@@ -18,12 +20,14 @@ def emotion_rec():
 		camera.start_recording(video_stream, format='h264', quality=23)
 		start_time = time.time()
 		while (time.time() - start_time) < 10:
-			if(lock.acquire(blocking=)):
-				camera.wait_recording(2)
-				camera.capture(image_stream, use_video_port=True, format='jpeg')
-				# Spawn a separate thread for the request so video is not blocked
-				lock.release()
-				Thread(target=make_request, args=(image_stream,)).start()
+			camera.wait_recording(2)
+			camera.capture(image_stream, use_video_port=True, format='jpeg')
+			if image_queue.full():
+				with image_queue.mutex:
+					image_queue.clear()
+			image_queue.put(image_stream)
+			image_stream.flush()
+			Thread(target=make_request).start()
 		camera.stop_recording()
 		camera.stop_preview()
 
@@ -34,24 +38,25 @@ def get_request_params():
 	request_params['request_data'] = {'returnFaceAttributes':'emotion,smile', 'overload':'stream'}
 	return request_params
 
-def make_request(image_stream):
+def make_request():
 	global file_counter
 	print ("Making_request")
-	lock_on_stream = lock.acquire()
 	request_params = get_request_params()
-	try:
-		image_stream.seek(0)
-		with open("test_file_{0}.jpeg".format(file_counter), "wb") as fp:
-			image_stream.seek(0)
-			file = image_stream.read()
-			fp.write(file)
-			file_counter+=1
-		image_stream.seek(0)
-		request_url = 'https://hackcambridge-emotiondetector.cognitiveservices.azure.com/face/v1.0/detect'
-		request = requests.post(request_url, params=request_params['request_data'], headers=request_params['headers'], data=image_stream)
-		print (request.json())
-	finally:
-		image_stream.flush()
-		lock_on_stream = lock.release()
+	if not image_queue.empty():
+		image_to_request = image_queue.get()
+		try:
+			with open("test_file_{0}.jpeg".format(file_counter), "wb") as fp:
+				image_stream.seek(0)
+				file = image_to_request.read()
+				fp.write(file)
+				file_counter+=1
+			image_to_request.seek(0)
+			request_url = 'https://hackcambridge-emotiondetector.cognitiveservices.azure.com/face/v1.0/detect'
+			request = requests.post(request_url, params=request_params['request_data'], headers=request_params['headers'], data=image_to_request)
+			print (request.json())
+		except:
+			print ("Try threw an error")
+
+
 
 emotion_rec()
